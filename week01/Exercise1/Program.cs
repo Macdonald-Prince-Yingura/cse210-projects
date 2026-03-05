dbd40736bd77100ed0cd80c5722c120827dfe895
Macdonald-Prince-Yingura <you@example.com>
2026-03-05 10:57:33 +0000

    completed week1 exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        //ask the user for their name.
+        Console.WriteLine("What is your first name?");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last},{first},{last}");
+        
     }
 }
